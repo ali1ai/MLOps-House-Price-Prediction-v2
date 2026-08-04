8797e3cd58c4b35e7faf262100edd96352ba7336
ggao9 <ggao9@myseneca.ca>
2026-08-04 15:56:54 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model import predict_sale_price
 
+
 app = FastAPI(
     title="Ames Housing Price Prediction API",
     description="Predict house prices using a CatBoost model",
     version="1.0"
 )
+
 
 # Define the input schema (must match your FEATURES list)
 class HouseFeatures(BaseModel):
